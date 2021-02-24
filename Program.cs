b26adb0889569dd43468c3ef03c849ef19f1e1ad
MykolaBerkosha <65126987+MykolaBerkosha@users.noreply.github.com>
2021-02-24 19:41:26 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Lab_2._2
+
+{
+    class Program
+    {
+        static void Main()
+        {
+            double y, x = 1, b = 6, dx = 0.2;
+            Console.WriteLine("X\t\tY");
+            while (x <= b)
+            {
+                if (x < 0.2) //пошук помилок
+                {
+                    Console.WriteLine(Math.Round(x, 2) + "\tЛагарифм з відємного числа");
+                }
+                else
+                {
+                    y = Math.Log10(x);
+                    Console.WriteLine(Math.Round(x, 2) + "\t\t" + Math.Round(x, 2));
+                }
+                x += dx;
+
+            }
+        }
+    }
+}
+
+            
+
+                
